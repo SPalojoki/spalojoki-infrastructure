46dbb30d0cee5ef05fb3e5801decedea0326e516
Sakari Palojoki <sakari.palojoki@kone.com>
2024-06-13 12:26:47 +0300

    Fix syntax with variables

--- bootstrap/variables.tf
+++ bootstrap/variables.tf
@@ -2,17 +2,17 @@ variable "gcp_project" {
     description = "GCP project id"
 }
 
-variable gcp_region {
+variable "gcp_region" {
     description = "GCP region"
     default = "europe-north1"
 }
 
-variable gcp_zone {
+variable "gcp_zone" {
     description = "GCP zone within the selected region"
     default = "europe-north1a"
 }
 
-variable spalojoki_infra_bucket {
+variable "spalojoki_infra_bucket" {
     description = "Name of the GCP bucket used to store remote Terraform state"
     default = "spalojoki-infrastructure-bucket"
 }
--- main/variables.tf
+++ main/variables.tf
@@ -2,22 +2,22 @@ variable "gcp_project" {
     description = "GCP project id"
 }
 
-variable gcp_region {
+variable "gcp_region" {
     description = "GCP region"
     default = "europe-north1"
 }
 
-variable gcp_zone {
+variable "gcp_zone" {
     description = "GCP zone within the selected region"
     default = "europe-north1a"
 }
 
-variable spalojoki_infra_bucket {
+variable "spalojoki_infra_bucket" {
     description = "Name of the GCP bucket used to store infra related files, for instance remote Terraform state"
     default = "spalojoki-infrastructure-bucket"
 }
 
-variable var.tf_state_bucket_path {
+variable "tf_state_bucket_path" {
     description = "File path the TF remote state is saved to in the GCS bucket"
     default = "terraform/state"
 }
