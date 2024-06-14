ed9860a090feb56d8ff96e402780cbe12201877c
Sakari Palojoki <sakari.palojoki@kone.com>
2024-06-14 10:42:32 +0300

    Enforce access rights in backend bucket

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -5,7 +5,34 @@ provider "google" {
 }
 
 resource "google_storage_bucket" "infrastructure_bucket" {
-  name     = var.spalojoki_infra_bucket
-  location = "EU"
-  force_destroy = true
+  name          = var.spalojoki_infra_bucket
+  location      = "EU"
+  force_destroy = false
+
+  public_access_prevention = "enforced"
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 365
+    }
+  }
+}
+
+resource "google_storage_bucket_iam_member" "terraform_state_writer" {
+  bucket   = google_storage_bucket.infrastructure_bucket.name
+  role     = "roles/storage.objectAdmin"
+  member   = "serviceAccount:${var.terraform_service_account_email}"
+}
+
+resource "google_storage_bucket_iam_member" "terraform_state_reader" {
+  bucket   = google_storage_bucket.infrastructure_bucket.name
+  role     = "roles/storage.objectViewer"
+  member   = "serviceAccount:${var.terraform_service_account_email}"
+}
+
+output "bucket_name" {
+  value = google_storage_bucket.infrastructure_bucket.name
 }
--- bootstrap/variables.tf
+++ bootstrap/variables.tf
@@ -11,8 +11,12 @@ variable "gcp_zone" {
     description = "GCP zone within the selected region"
     default = "europe-north1a"
 }
-
 variable "spalojoki_infra_bucket" {
     description = "Name of the GCP bucket used to store remote Terraform state"
     default = "spalojoki-infrastructure-bucket"
+}
+
+variable "terraform_service_account_email" {
+  description = "The email of the service account to be granted access to the bucket"
+  type        = string
 }
